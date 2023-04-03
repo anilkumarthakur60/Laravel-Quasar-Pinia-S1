86207c48c08ae279da45ea01741d0472684132ff
Anil <anilkumarthakur60@gmail.com>
2023-04-03 21:55:52 +0545

    template setup

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,51 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
-</script>
-
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+    <div class="">
+        <div class="q-pa-md">
+          <div class="q-gutter-y-md">
+            <q-card>
+              <q-tabs
+                  v-model="tab"
+                  dense
+                  class="text-grey"
+                  active-color="primary"
+                  indicator-color="primary"
+                  align="justify"
+                  narrow-indicator
+              >
+                <q-tab name="mails" label="Mails"/>
+                <q-tab name="alarms" label="Alarms"/>
+                <q-tab name="movies" label="Movies"/>
+              </q-tabs>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+              <q-separator/>
+
+              <q-tab-panels v-model="tab" animated>
+                <q-tab-panel name="mails">
+                  <div class="text-h6">Mails</div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </q-tab-panel>
+
+                <q-tab-panel name="alarms">
+                  <div class="text-h6">Alarms</div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </q-tab-panel>
+
+                <q-tab-panel name="movies">
+                  <div class="text-h6">Movies</div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </q-tab-panel>
+              </q-tab-panels>
+            </q-card>
+
+
+          </div>
         </div>
-    </AppLayout>
+    </div>
 </template>
+
+
+<script setup>
+import {ref} from "vue";
+import AppLayout from '@/Layouts/AppLayout.vue';
+const tab = ref('mails');
+</script>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+import svgLoader from "vite-svg-loader";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -15,6 +18,10 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+         svgLoader(),
+        quasar({
+            autoImportComponentCase: "combined",
         }),
     ],
 });
